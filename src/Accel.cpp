5fb6ba2179379f20a1fef79570a74911fc5848ef
krashanoff <lkrashanoff2@gmail.com>
2020-01-11 19:00:42 -0800

    Update accelerometer interface.

--- src/Accel.cpp
+++ src/Accel.cpp
@@ -4,18 +4,39 @@
 Accel::Accel(int power, int ground, int x, int y, int z)
       : m_powerPin(power), m_ground(ground), m_x(x), m_y(y), m_z(z)
 {
-      pinMode(m_powerPin, OUTPUT);
-      pinMode(m_ground, OUTPUT);
-      digitalWrite(m_ground, LOW);
-      digitalWrite(m_powerPin, HIGH);
+    pinMode(m_powerPin, OUTPUT);
+    pinMode(m_ground, OUTPUT);
+    digitalWrite(m_ground, LOW);
+    digitalWrite(m_powerPin, HIGH);
 }
 
-int* Accel::read()
+// read from the accelerometer and update its readings.
+void Accel::updateAccel()
 {
-      int a = analogRead(m_x);
-      int b = analogRead(m_y);
-      int c = analogRead(m_z);
-
-      int res[3] = {a, b, c};
-      return res;
+    // TODO: Read from accelerometer, update readings and previous reading.
 }
+
+// return an array of three integers, being the
+// readings for the x, y, and z acceleration
+// of the accelerometer at the current time.
+int* Accel::readAccel() const
+{
+    int a = analogRead(m_x);
+    int b = analogRead(m_y);
+    int c = analogRead(m_z);
+
+    int res[3] = {a, b, c};
+    return res;
+}
+
+// returns whether the accelerometer has moved a "big" amount
+// since the past update.
+bool Accel::bigShift() const
+{
+    return abs((m_prevX - m_x)) > HIT_THRESHOLD ||
+           abs((m_prevY - m_y)) > HIT_THRESHOLD ||
+           abs((m_prevZ - m_z)) > HIT_THRESHOLD;
+}
+
+// absolute value.
+int Accel::abs(int i) const { return i < 0 ? i*-1 : i; }
--- src/Accel.h
+++ src/Accel.h
@@ -1,18 +1,29 @@
 #ifndef ACCEL_H_
 #define ACCEL_H_
 
+// Threshold beyond which a hit is registered.
+#define HIT_THRESHOLD 200
+
+// Accel represents a hardware accelerometer.
 class Accel
 {
 public:
     Accel(int, int, int, int, int);
-    int* read();
+    void updateAccel();
+    int* readAccel() const;
+    bool bigShift() const;
 
 private:
     int m_powerPin;
     int m_ground;
+    int m_prevX;
+    int m_prevY;
+    int m_prevZ;
     int m_x;
     int m_y;
     int m_z;
+
+    int abs(int) const;
 };
 
 #endif  // ACCEL_H_
